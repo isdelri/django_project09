0d0d24ac49f9308efd9ed92ac81f0d41a91073f1
Isaac Del Rio <isaac.delrio@outlook.com>
2024-12-10 03:07:33 -0800

    Fix Procfile and static files

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -1,22 +1,23 @@
 import os
 import dj_database_url
-
 from .settings import *
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="sqlite:///" + os.path.join(BASE_DIR, "sb.sqlite3")
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
     )
 }
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALLOWED_HOSTS = ["*"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 MIDDLEWARE = (
     "whitenoise.middleware.WhiteNoiseMiddleware",
     *MIDDLEWARE,
 )
-
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,3 @@
-# mysite/wsgi.py
-
 import os
 from django.core.wsgi import get_wsgi_application
 
